55256003005a74c696029cb669ba5275d4d9d130
Maxime Fabre <ehtnam6@gmail.com>
2014-01-25 21:13:56 +0100

    Setup tests

---
+++ tests/SpliceTestCase.php
@@ -0,0 +1,12 @@
+<?php
+namespace Splice;
+
+use PHPUnit_Framework_TestCase;
+
+/**
+ * Basic test case for all Splice tests
+ */
+abstract class SpliceTestCase extends PHPUnit_Framework_TestCase
+{
+	// ...
+}
